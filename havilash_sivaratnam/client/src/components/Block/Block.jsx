b7ae0855790e1bda5b2dfd3bdce64b05b905e0ee
bsivah@bbcag.ch <bsivah@ws-sr5-17.bbc-be.local>
2023-04-20 16:38:30 +0200

    update

--- havilash_sivaratnam/client/src/components/Block/Block.jsx
+++ havilash_sivaratnam/client/src/components/Block/Block.jsx
@@ -2,22 +2,23 @@ import React, { useEffect, useState } from 'react'
 import { FaAngleDown } from 'react-icons/fa'
 
 export default function Block({children, className, title, subtitle, open}) {
-    const [isOpen, setIsOpen] = useState(open || true)
-
-    // useEffect(() => {
-        
-    // }, [isOpen])
+    const [isOpen, setIsOpen] = useState(open || false)
+    console.log(isOpen)
 
   return (
     <div className={`bg-block-color rounded-lg w-1/3 h-3/5 ${className}`}>
         <div className='shadow-lg p-4 flex flex-row content-center justify-between relative'>
             <h1 className='text-3xl text-white'>{title}</h1>
-            <p className='absolute bottom-1 text-sm'>{subtitle}</p>
+            <p onClick={() => setIsOpen(isOpen)} className='absolute bottom-1 text-sm'>{subtitle}</p>
             <FaAngleDown size="2.5rem"/>
         </div>
-        <div>
-            {children}
-        </div>
+        {
+            isOpen && (
+                <div>
+                    {children}
+                </div>
+            )
+        }
     </div>
   )
 }
