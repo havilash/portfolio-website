6f4c7868bb99a428e759977604c06bb9ed217cc3
Havilash Sivaratnam <havilash2005@gmail.com>
2023-08-17 07:14:32 +0200

    update skeleton

--- havilash_sivaratnam/frontend/src/components/skeletons/SkeletonFile/SkeletonFile.jsx
+++ havilash_sivaratnam/frontend/src/components/skeletons/SkeletonFile/SkeletonFile.jsx
@@ -3,17 +3,17 @@ import Skeleton from "src/components/skeletons/Skeleton/Skeleton";
 export default function SkeletonFile({ className }) {
   return (
     <div
-      className={`bg-white w-full p-[2.5cm] pb-[2cm] flex flex-col gap-4 ${className}`}
+      className={`bg-white w-full p-[15%] pb-[10%] flex flex-col gap-4 ${className}`}
       style={{ aspectRatio: "1/1.414" }}
     >
-      <Skeleton width="30%" height="2em" className="mb-4" />
-      <Skeleton width="90%" />
-      <Skeleton width="80%" />
-      <Skeleton width="88%" />
-      <Skeleton width="70%" />
-      <Skeleton width="90%" />
-      <Skeleton width="85%" />
-      <Skeleton width="60%" />
+      <Skeleton width="30%" height="5%" className="mb-[2%]" />
+      <Skeleton width="90%" height="3%" />
+      <Skeleton width="80%" height="3%" />
+      <Skeleton width="88%" height="3%" />
+      <Skeleton width="70%" height="3%" />
+      <Skeleton width="90%" height="3%" />
+      <Skeleton width="85%" height="3%" />
+      <Skeleton width="60%" height="3%" />
     </div>
   );
 }
